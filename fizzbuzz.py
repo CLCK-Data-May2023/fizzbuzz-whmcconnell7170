c6ce245e0eb869b81275268711e4f09e3048fefd
unknown <whmcconnell@protonmail.com>
2023-07-18 16:58:05 -0400

    added assignment

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,12 @@
 # add your code here
-
+list = range(1,101,1)
+for i in list:
+    #print(i)
+    if i % 3 == 0 and i % 5 == 0:
+        i = 'FizzBuzz'
+    #print(i)
+    elif i % 3 == 0:
+        i = 'Fizz'
+    elif i % 5 == 0:
+        i = 'Buzz'
+    print(i)
